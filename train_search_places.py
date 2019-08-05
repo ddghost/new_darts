a49fdd2796539ee99bcc3f110b3def51e1dc1430
ddghost <544011312@qq.com>
2019-08-05 11:38:16 +0800

    update

--- train_search_places.py
+++ train_search_places.py
@@ -195,8 +195,8 @@ def infer(valid_queue, model, criterion):
   for step, (input, target) in enumerate(valid_queue):
     #input = input.cuda()
     #target = target.cuda(non_blocking=True)
-    input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+    target = target.cuda(non_blocking=True)
+    input = input.cuda(non_blocking=True)
     logits = model(input)
     loss = criterion(logits, target)
 
