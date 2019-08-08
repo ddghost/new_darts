6e9d326c4033009ba9dd7d96f3978dbce7b8ab6c
ddghost <544011312@qq.com>
2019-08-08 17:28:49 +0800

    update

--- genotypes.py
+++ genotypes.py
@@ -2,5 +2,15 @@ from collections import namedtuple
 
 Genotype = namedtuple('Genotype', 'normal_stage1 normal_stage2 normal_stage3 normal_concat reduce reduce_concat')
 
+PRIMITIVES = [
+    'none',
+    'max_pool_3x3',
+    'avg_pool_3x3',
+    'skip_connect',
+    'sep_conv_3x3',
+    'sep_conv_5x5',
+    'dil_conv_3x3',
+    'dil_conv_5x5'
+]
 
 
