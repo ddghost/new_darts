dd317308196c6545a92e96998d0b386ddf2b74f3
ddghost <544011312@qq.com>
2019-08-18 14:00:30 +0800

    update

--- SENet.py
+++ SENet.py
@@ -1,10 +1,15 @@
 import torch.nn as nn
 import math
 
-def conv3x3(in_planes, out_planes, stride=1):
-    "3x3 convolution with padding"
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 class SELayer(nn.Module):
     def __init__(self, channel, reduction=16):
